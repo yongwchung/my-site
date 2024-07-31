af65ce1f1c872e8bf40bdc393120659327497981
Yong Chung <ywc005@ucsd.edu>
2024-07-30 17:06:32 -0700

    Fixing vite config root directory and associated css files

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider, Route, } from 'react-router-dom'
 import Home from './Pages/Home/Home.jsx'
@@ -40,7 +39,7 @@ const router = createBrowserRouter([
     path: 'contact',
     element:<Contact/>,
   },
-]);
+], {basename: "/my-site"});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
